1646bcc8f156bf54b93c11494fe31724d92b6e69
limintae@gmail.com <limintae@gmail.com>
2022-01-07 00:26:26 +0900

    프로그래머스 체육복 문제풀이

--- src/main/java/programmers/greedy/GymSuit.java
+++ src/main/java/programmers/greedy/GymSuit.java
@@ -1,45 +1,60 @@
 package programmers.greedy;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GymSuit {
 
-    public static void main(String[] args) {
-        System.out.println(solution(5, new int[] {2,4},new int[] {1,3,5}));
-//        System.out.println(solution(5, new int[] {2,4},new int[] {3}));
-    }
+//    public static void main(String[] args) {
+//        System.out.println(solution(5, new int[] {2,4},new int[] {1,3,5}));
+////        System.out.println(solution(5, new int[] {2,4},new int[] {3}));
+//    }
 
-    private static int solution(int n, int[] lost, int[] reserve) {
+    public static int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
         int answer = 0;
+        int lostCount = lost.length;
+        int lostAndReverseCount = 0;
 
+        // 체육복을 빌려줄 수 있는 학생 List
         List<Integer> removeLostReserve = new ArrayList<>();
+
         for (int i = 0; i < reserve.length; i++) {
             boolean isDup = false;
-            for(int j = 0; j < lost.length; j++) {
+            for (int j = 0; j < lost.length; j++) {
                 if (reserve[i] == lost[j]) {
                     isDup = true;
+                    lost[j] = -1;
                     break;
                 }
             }
             if (!isDup) {
                 removeLostReserve.add(reserve[i]);
+            } else {
+                lostAndReverseCount += 1;
             }
         }
 
+        Set<Integer> getterSuitLostStudents = new HashSet<>();
+        List<Integer> lostStudents = new ArrayList<>();
         for (Integer i : removeLostReserve) {
             for (int j = 0; j < lost.length; j++) {
-                if (i + 1 == lost[j]) {
+                int lostStudentNumber = lost[j];
+                if (getterSuitLostStudents.contains(lostStudentNumber) || lostStudentNumber == -1) {
+                    continue;
+                }
+                if (i + 1 == lostStudentNumber || i - 1 == lostStudentNumber) {
+                    getterSuitLostStudents.add(lostStudentNumber);
+                    lostCount = lostCount - 1;
                     break;
                 }
-                if (i - 1 == lost[j]) {
-                    break;
-                }
-                answer += 1;
+                lostStudents.add(lostStudentNumber);
             }
         }
 
-        return n - answer;
+        answer = n - lostCount + lostAndReverseCount;
+        return answer;
     }
 
 }
---
+++ src/test/java/ProgrammersTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import programmers.greedy.GymSuit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProgrammersTest {
+
+    @Test
+    @DisplayName("[Greedy] 체육복")
+    void gymSuitTest() {
+//        assertThat(GymSuit.solution(5, new int[] {2,4},new int[] {1,3,5})).isEqualTo(5);
+//        assertThat(GymSuit.solution(5, new int[] {2,4},new int[] {3})).isEqualTo(4);
+//        assertThat(GymSuit.solution(3, new int[] {3},new int[] {1})).isEqualTo(2);
+//        assertThat(GymSuit.solution(4, new int[] {4,2},new int[] {3,5})).isEqualTo(4);
+        assertThat(GymSuit.solution(3, new int[] {1,2},new int[] {2,3})).isEqualTo(2);
+    }
+
+}
