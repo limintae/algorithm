f4f1acc809c6805d2816e613e419b3fdb32d4c5e
limintae@gmail.com <limintae@gmail.com>
2022-02-06 19:12:16 +0900

    주석 수정

---
+++ src/main/java/kakao/P92342.java
@@ -0,0 +1,18 @@
+package kakao;
+
+/**
+ * 양궁대회
+ * https://programmers.co.kr/learn/courses/30/lessons/92342
+ */
+public class P92342 {
+
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        boolean[] visited = new boolean[info.length];
+
+        return answer;
+    }
+
+//    void dfs(int n, int depth, )
+
+}
--- src/main/java/programmers/dfs/Network.java
+++ src/main/java/programmers/dfs/Network.java
@@ -12,6 +12,7 @@ public class Network {
 
         for (int i = 0; i < n; i++) {
             if (!checks[i]) {
+                // 방문하지 않은 노드만 탐색
                 dfs(computers, i, checks);
                 answer++;
             }
@@ -23,6 +24,8 @@ public class Network {
         check[i] = true;
         for (int j = 0; j < computers.length; j++) {
             if (i != j && computers[i][j] == 1 && !check[j]) {
+                // 자기 자신 index는 제외, 노드의 네트워크가 연결되어있음, 아직방문하지 않았을 경우
+                // 노드를 방문한다
                 check = dfs(computers, j, check);
             }
         }
